c17841d4f87254f571aee56cb1e87bc8b4d36645
Pushkar <pandeypushkar809@gmail.com>
2025-11-15 14:56:20 +0530

    Added changes

--- network.tf
+++ network.tf
@@ -50,3 +50,72 @@ resource "aws_subnet" "db" {
 
 }
 
+resource "aws_internet_gateway" "base" {
+  vpc_id = aws_vpc.base.id
+  tags = {
+    Name = "from-tf"
+    Env  = "Dev"
+
+  }
+
+
+  depends_on = [aws_vpc.base]
+}
+
+
+
+resource "aws_route_table" "private" {
+  vpc_id = aws_vpc.base.id
+
+  tags = {
+    Name = "private"
+    Env  = "Dev"
+
+  }
+
+  depends_on = [aws_internet_gateway.base, aws_subnet.db, aws_subnet.app]
+}
+
+
+resource "aws_route_table" "public" {
+  vpc_id = aws_vpc.base.id
+
+  tags = {
+    Name = "public"
+    Env  = "Dev"
+
+  }
+
+  depends_on = [aws_subnet.web, aws_subnet.app]
+}
+
+
+
+
+resource "aws_route" "internet" {
+  destination_cidr_block = "0.0.0.0/0"
+  gateway_id             = aws_internet_gateway.base.id
+  route_table_id         = aws_route_table.public.id
+  depends_on             = [aws_internet_gateway.base, aws_route_table.public]
+}
+
+
+resource "aws_route_table_association" "public" {
+  route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.web.id
+
+}
+
+
+resource "aws_route_table_association" "private_app" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.app.id
+}
+
+resource "aws_route_table_association" "private_db" {
+  route_table_id = aws_route_table.private.id
+  subnet_id      = aws_subnet.db.id
+}
+
+
+
